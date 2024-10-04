1f1f22822cddb40f1a81701d679427358130176c
Leonardo Souza <leonardomellolopes@gmail.com>
2024-10-03 21:53:20 -0300

    second Commit

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
-const formulario = document.getElementById("form")
+const formulario = document.getElementById("form");
 
 
 // adiciona o evento para manipular o formulario ao botao cadastrado 
 const btnCadastrar = document.getElementById("btnCadastrar")
-const inputNome = document.getElementById("nome")
+
+const inputNome = document.getElementById("name")
 const inputEmail = document.getElementById("email")
-const inputSenha = document.getElementById("senha")
+const inputSenha = document.getElementById("password")
+
+const listaPessoa = []
 
 function cadastrar(event) {
-event.preventDefault()
+    event.preventDefault()
+    
+    const nome = inputNome.value
+    const email = inputEmail.value
+    const senha = inputSenha.value
 
-const nome = inputNome.value
-const email = inputEmail.value
-const senha = inputSenha.value
+    // Validação, verificando se os campos tem conteúdo 
 
-console.log ({
-    nome : nome,
-    email: email, 
-    senha: senha
-})
+    if (!(nome.trim())) {
+        alert("Informe seu nome")
+        inputNome.focus
+        return
 
-}
+    }
 
-const cadastrarAnonimo = (event) => {
+    if (!(email.trim())) {
+        alert("Informe seu email")
+        inputEmail.focus
+        return
+    }
+    if (!(senha.trim())) {
+        alert("Informe sua senha")
+        inputSenha.focus
+        return
+    }
+    // Caso os dados estejam válidos, procura na lista 
+    // Se ja existe um cadastro com o mesmo email 
+
+    // fo/each e fazer a comparação 
+
+    let pessoaEncontrada = null
+
+    for (pessoa of listaPessoas) {
+        if (pessoa.email === email.toLowerCase()) {
+            pessoaEncontrada = pessoa
+            break
+
+        }
+    }
+
+
+// Caso não exista, adiciona um array 
+    console.log({
+      nome: nome,
+      email: email,
+      senha: senha
+    })
+  }
+  
+  const cadastrarAnonimo = (event) => {
     console.log(event)
-}
-
-btnCadastrar.addEventListener('click', cadastrar )
+  }
+  
+  btnCadastrar.addEventListener('click', cadastrar)
 
